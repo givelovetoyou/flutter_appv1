0f2e28f5a9ed58564aa7522aeea499b294de6767
Matheus Figueirêdo <30707765+matheustgf10@users.noreply.github.com>
2020-10-02 15:00:34 -0300

    The class main.dart was splited in new files

---
+++ lib/input_with_icon.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class InputWithIcon extends StatefulWidget {
+  final IconData icon;
+  final String hint;
+  InputWithIcon({this.icon, this.hint});
+  @override
+  _InputWithIconState createState() => _InputWithIconState();
+}
+
+class _InputWithIconState extends State<InputWithIcon> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: Color(0xFFBC7C7C7), width: 1),
+          borderRadius: BorderRadius.circular(50)),
+      child: Row(
+        children: <Widget>[
+          Container(
+              width: 60,
+              child: Icon(
+                widget.icon,
+                size: 20,
+                color: Color(0xFFBB9B9B9),
+              )),
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: widget.hint,
+                  contentPadding: EdgeInsets.symmetric(vertical: 20)),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
